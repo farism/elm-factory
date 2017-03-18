c736acd049297b45b63d5f41b05b8ea346b143f5
Faris Mustafa <farismmk@gmail.com>
2017-03-18 14:54:17 -0700

    use proxy package, get custom template working

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+const fs = require('fs')
 const child_process = require('child_process')
+const path = require('path')
 const chokidar = require('chokidar')
 const express = require('express')
 const request = require('request-promise')
+const proxy = require('http-proxy-middleware')
 const findElmDeps = require('find-elm-dependencies')
 
 const ENTRY = './src/Stylesheets.elm'
+
+const extractCss = () => {
+  const output = path.join(process.cwd(), 'dist')
+  const module = 'Stylesheets'
+  const port = 'files'
+  const root_ = ''
+
+  // const start = new Date().getTime()
+
+  child_process.exec(
+    `elm-css ${ENTRY} -o ${output} -m ${module} -p ${port} -r ${root_}`
+  )
+
+  // console.log(new Date().getTime() - start)
+}
 
 const getElmDependencies = async (entry) => {
   try {
@@ -20,15 +38,19 @@ const addOnChange = async (entry, watcher) => {
   watcher.on('change', async (path) => {
     console.log('changed: ', path)
 
-    const newFiles = await getElmDependencies(entry)
+    // close watcher and clear all listeners
+    watcher.close()
 
-    try {
-      watcher.close()
-      addOnChange(entry, watcher)
-      watcher.add([entry, ...newFiles])
-    } catch (e) {
-      console.log(e)
-    }
+
+    // readd on change listener
+    addOnChange(entry, watcher)
+
+    // get new dep tree and add it to watch
+    const newFiles = await getElmDependencies(entry)
+    watcher.add([entry, ...newFiles])
+
+    // extract css to a file
+    extractCss()
   })
 }
 
@@ -45,81 +67,53 @@ const initWatcher = async (entry) => {
   addOnChange(entry, watcher)
 }
 
-initWatcher(ENTRY)
-
-// start css watching
-// addElmDependencies(entry, watcher)
-
 // init express server
 const app = new express()
 
-// catch all route, proxied first to elm-reactor server
-// and then to a user defined proxy
-app.get('*', function(req, res) {
-  requestFromElmReactor(req, res)
-    .catch(reactorError => {
-      console.log(`reactor ${reactorError.statusCode}:`, req.url)
+app.use(proxy('/_compile', {
+  target: 'http://localhost:8001',
+}))
 
-      return requestFromProxy(req, res)
-    })
-    .catch(proxyError => {
-      console.log(`proxy ${proxyError.statusCode}:`, req.url)
-
-      res.send(proxyError)
-    })
+app.get('*.elm', (req, res) => {
+  res.send(fs.readFileSync('index.html').toString())
 })
 
-const requestFromElmReactor = (req, res) => {
-  return request(`http://localhost:8001${req.url}`)
-    .then(reactorResponse => {
-      console.log('reactor 200:', req.url)
+app.use(proxy({
+  target: 'http://localhost:8001',
+}))
 
-      res.send(reactorResponse)
-    })
-}
+// app.use(proxy('/api', {
+//   target: 'http://localhost:8001',
+// }))
 
-const requestFromProxy = (req, res) => {
-  return request(`http://localhost:3000${req.url}`)
-    .then(proxyResponse => {
-      console.log('proxy 200:', req.url)
-
-      res.send(proxyResponse)
-    })
-}
-
+// start watching css
+// initWatcher(ENTRY)
 
 // start elm-reactor
-// const reactor = child_process.spawn('elm-reactor', [
-//   '--address=127.0.0.1',
-//   '--port=8001',
-// ])
-
-// console.log('here', reactor.stderr)
+const reactor = child_process.spawn('elm-reactor', [
+  '--address=127.0.0.1',
+  '--port=8001',
+])
 
 // start express
-// app.listen(8000, () => {
-//   console.log('elm-reactor-proxy listening on port 8001!')
-// })
+app.listen(8000, () => {
+  console.log('elm-reactor-proxy listening on port 8000!')
+})
 
+// catch exiting
+process.on('exit', code => {
+  reactor.kill('SIGINT')
+  process.exit(code)
+})
 
+// catch ctrl+c
+process.on('SIGINT', () => {
+  reactor.kill('SIGINT')
+  process.exit(0)
+})
 
-
-
-//
-//
-// process.on('exit', code => {
-//   reactor.kill('SIGINT')
-//   process.exit(code)
-// })
-//
-// // catch ctrl+c
-// process.on('SIGINT', () => {
-//   reactor.kill('SIGINT')
-//   process.exit(0)
-// })
-//
-// // catch uncaught exception
-// process.on('uncaughtException', err => {
-//   reactor.kill('SIGINT')
-//   process.exit(1)
-// })
+// catch uncaught exception
+process.on('uncaughtException', err => {
+  reactor.kill('SIGINT')
+  process.exit(1)
+})
