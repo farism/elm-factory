d920d7dddec8dae7cf489b1a4ac26b9a0a69e50e
Faris Mustafa <farismmk@gmail.com>
2017-08-09 00:46:36 -0700

    cleanup crap

--- vendor_dir/assets/elm/NoRedInk/elm-asset-path-2.0.0/tests/Ports.elm
+++
@@ -1,6 +0,0 @@
-port module Ports exposing (emit)
-
-import Json.Encode exposing (Value)
-
-
-port emit : ( String, Value ) -> Cmd msg
--- vendor_dir/assets/elm/NoRedInk/elm-asset-path-2.0.0/tests/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-mkdir -p build
-../node_modules/.bin/elm-make WithRootUrlTest.elm --output build/with_root_url_test.js $@
-../node_modules/.bin/elm-make NoRootUrlTest.elm --output build/no_root_url_test.js $@
